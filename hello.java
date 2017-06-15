652a31af9c6385a5f677c3a47070fea7203d69e3
rohini1996 <rohini111222@gmail.com>
2017-06-15 17:37:34 +0530

    hello.java

---
+++ hello.java
@@ -0,0 +1,13 @@
+import java.io.*;
+import java.util.Scanner;
+class hello
+{
+public static void main(String args[])
+{
+String s="Hello";
+for(int i=1;i<=5;i++)
+{
+System.out.println(s);
+}
+}
+}
